4a74dd634cf0f53747c4b965c9a9d757c30507cd
Dan Odin <danphilodin@gmail.com>
2021-04-27 01:55:07 +0100

    Overflow Fix

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class HelloWorldApp extends StatelessWidget {
                 ),
               ),
               Expanded(
-                child: Row( // this is the row, it has 4 containers inside
+                child: Row( // this is the row, it has 4 containers inside.
                   children: [
                         Expanded(
                           child: Container( // container 1
@@ -178,3 +178,21 @@ class HelloWorldApp extends StatelessWidget {
     );
   }
 }
+
+/*
+Author: DanOdin
+Project: Overflow error in row
+For: Zuri Training
+Genre: Bug Hunting
+Product Type: Course task
+Test Area: Web in another fork[Unable to run emulators, build time to mobile device is long, pc is weak]
+Date Written: 27-April-2021
+Date Completed: 27-April-2021
+Start Time - End Time: 10:45 pm - 1:45PM
+
+How was it solved: Following flutter video by abbie, simply put the containers in
+the row in an Expanded() widget like she did with other containers and problem solved.
+
+Note: :) in excitement about my first task in flutter, i made it colourful
+hope that doesn't cause any troubles. .
+*/
